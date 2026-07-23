66e5da0fc5ef3df82fb150df2035136640eec90a
Ramesh Pandey <rameshkpandey1889@gmail.com>
2026-07-23 11:34:27 +0530

    Create Employee entity

---
+++ employee-service/src/main/java/com/sourcekoza/employeeservice/entity/Employee.java
@@ -0,0 +1,44 @@
+package com.sourcekoza.employeeservice.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
